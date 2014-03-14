f98f31b85aac6fc06802c608811496f172f8291a
Robin Burchell <robin+git@viroteck.net>
2014-03-15 00:30:11 +0100

    rewrite job handling
    this now attempts to keep as many jobs compiling as possible
    it also detects errors during compilation

--- tbs.cpp
+++ tbs.cpp
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <unistd.h>
 
+#include <map>
 #include <string>
 #include <vector>
 #include <iterator>
@@ -77,57 +78,71 @@ int main(int argc, char **argv)
     std::vector<std::string> cfiles = d.source_files();
     std::vector<std::string> ccopy = cfiles;
 
-    // TODO: optimize for the case where there's only a single file and don't
-    // generate a .o
-    while (ccopy.size()) {
-        std::vector<int> cfds;
-        fd_set readfds;
+    int maxjobs = 2;
+    std::map<int, std::string> curjobs;
 
-        FD_ZERO(&readfds);
+    // while there are things to build
+    while (ccopy.size() || curjobs.size()) {
+        // check status on existing builds (if any)
+        if (curjobs.size() != 0) {
+            fd_set readfds;
+            FD_ZERO(&readfds);
 
-        int spawnjobs = std::min(2 /* TODO: hardcoding */, (int)ccopy.size());
+            for (auto it = curjobs.cbegin(); it != curjobs.cend(); ++it)
+                FD_SET(it->first, &readfds);
 
-        for (int i = 0; i < spawnjobs; ++i) {
-            std::string name = ccopy[i];
+            int fds = 0;
+
+            // monitor until all compiles are done
+            do {
+                fds = select(FD_SETSIZE, &readfds, NULL, NULL, NULL);
+            } while (fds == -1 && errno == EINTR);
+
+            // close completed jobs
+            for (auto it = curjobs.begin(); it != curjobs.end();) {
+                if (FD_ISSET(it->first, &readfds)) {
+                    siginfo_t info;
+                    int bread = read(it->first, &info, sizeof(info));
+                    if (bread != sizeof(info)) {
+                        perror("can't get siginfo_t struct from forkfd");
+                        return -1;
+                    }
+
+                    printf("fd %d (job %s) had return code %d\n", it->first, it->second.c_str(), info.si_status);
+
+                    if (info.si_status != 0) {
+                        printf("error compiling %s\n", it->second.c_str());
+                        return -1;
+                    }
+
+                    close(it->first);
+                    it = curjobs.erase(it);
+                } else {
+                    it++;
+                }
+            }
+        }
+
+        // start builds
+        while (curjobs.size() < maxjobs && ccopy.size()) {
+            // take a job
+            std::string name = ccopy.back();
+            ccopy.pop_back();
+
             int compile_fd = builder::compile(name);
             if (compile_fd == -1) {
                 printf("forkfd for compile job %s failed\n", name.c_str());
                 return -1;
             }
 
-            printf("compiling %s, job %d, compile_fd %d\n", name.c_str(), i, compile_fd);
-            FD_SET(compile_fd, &readfds);
-            printf("compilation of %s: %d\n", name.c_str(), compile_fd);
+            curjobs[compile_fd] = name;
+            printf("compiling %s, compile_fd %d\n", name.c_str(), compile_fd);
         }
-
-        int fds = 0;
-
-        // monitor until all compiles are done
-        do {
-            fd_set read_copy = readfds;
-            fds = select(FD_SETSIZE, &read_copy, NULL, NULL, NULL);
-//            printf("%d compiles finished, want %d, err %s\n", fds, cfiles.size(), strerror(errno));
-        } while (fds != spawnjobs);
-
-        // close completed jobs
-        // TODO: start new jobs as old ones finish
-        for (int i = 0; i < FD_SETSIZE; ++i) {
-            if (FD_ISSET(i, &readfds)) {
-                siginfo_t info;
-                int bread = read(i, &info, sizeof(info));
-                if (bread != sizeof(info)) {
-                    perror("can't get siginfo_t struct from forkfd");
-                    return -1;
-                }
-
-                printf("fd %d had return code %d\n", i, info.si_status);
-
-                close(i);
-            }
-        }
-
-        ccopy.erase(ccopy.begin(), ccopy.begin() + spawnjobs);
     }
+
+
+    // TODO: optimize for the case where there's only a single file and don't
+    // generate a .o
 
     printf("linking %s\n", target.c_str());
     bool linked_ok = builder::link(target, cfiles);
