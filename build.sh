62ba4d7a704fd397635b396ad5cbdab674935b1a
Robin Burchell <robin+git@viroteck.net>
2014-04-08 23:40:15 +0200

    tbs: rename to main.cpp

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-g++ -O0 -ggdb -o tbs builder.cpp forkfd.c futils.cpp scanner.cpp target.cpp tbs.cpp global_options.cpp
+g++ -O0 -ggdb -o tbs builder.cpp forkfd.c futils.cpp scanner.cpp target.cpp global_options.cpp main.cpp
